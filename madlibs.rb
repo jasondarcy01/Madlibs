825cc8ed0aa096fc127274f605ab171d25b2c83a
darchcruise <darchcruise@yahoo.com>
2013-06-18 12:08:01 -0400

    madlibs file

---
+++ madlibs.rb
@@ -0,0 +1,16 @@
+puts "give me a name"
+name = gets.chomp
+
+puts "give me an age"
+age = gets.chomp
+
+puts "give me a city"
+city = gets.chomp
+
+puts "give me a state"
+state = gets.chomp
+
+puts "give me a verb in past tense"
+verb = gets.chomp
+
+puts "#{name} #{age} an old man from #{city} #{state} #{verb}."
